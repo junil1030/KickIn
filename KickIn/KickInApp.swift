dd29f4b70f70d8195169efbce2a6ec27fd3b6fb2
junil1030 <dccrdseo@naver.com>
2025-12-16 07:52:32 +0900

    first commit

---
+++ KickIn/KickInApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickInApp.swift
+//  KickIn
+//
+//  Created by 서준일 on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KickInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KickInTests/KickInTests.swift
@@ -0,0 +1,17 @@
+//
+//  KickInTests.swift
+//  KickInTests
+//
+//  Created by 서준일 on 12/10/25.
+//
+
+import Testing
+@testable import KickIn
+
+struct KickInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
