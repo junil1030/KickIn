844cf2d3044d159902b6a48526a8588e1a70016c
junil1030 <dccrdseo@naver.com>
2025-12-16 10:28:40 +0900

    feat: LogRouter 구성

--- KickIn/Core/Network/Log/Routers/LogRouter.swift
+++ KickIn/Core/Network/Log/Routers/LogRouter.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import Alamofire
+
+enum LogRouter: APIRouter {
+    case log
+    
+    var baseURL: URL {
+        if let url = URL(string: APIConfig.baseURL) {
+            return url
+        } else {
+            assert(false, "is not valid Log URL")
+        }
+    }
+    
+    var method: HTTPMethod {
+        return switch self {
+        case .log: .get
+        }
+    }
+    
+    var path: String {
+        return switch self {
+        case .log: "/log"
+        }
+    }
+    
+    var headers: HTTPHeaders {
+        switch self {
+        default:
+            let headerTypes: [APIHeader] = [.applicationJSON, .apiKey,]
+            return HTTPHeaders(headerTypes.map(\.httpHeader))
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        var components = URLComponents(string: self.baseURL.appendingPathComponent(self.path).absoluteString)!
+        
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.method = self.method
+        request.headers = self.headers
+        return request
+    }
+    
+    
+}
