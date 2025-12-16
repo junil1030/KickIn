a95e65ca6afcfcdbfba713f65e07113e705383c0
junil1030 <dccrdseo@naver.com>
2025-12-16 10:13:42 +0900

    feat: Bundle에서 APIKey, Domain 불러온 후 base URL 제공

--- KickIn/Core/Network/Helper/APIConfig.swift
+++ KickIn/Core/Network/Helper/APIConfig.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+enum APIConfig {
+    private static let info = Bundle.main.infoDictionary
+    
+    private static let domain: String = {
+        guard let domain = info?["domain"] as? String else {
+            fatalError("required base_url")
+        }
+        return domain
+    }()
+
+    static let apikey: String = {
+        guard let apiKey = info?["api_key"] as? String else {
+            fatalError("required api_key")
+        }
+        return apiKey
+    }()
+    
+    static let baseURL: String = "http://" + domain + "/v1"
+    static let socketURL: String = "http://" + domain
+}
