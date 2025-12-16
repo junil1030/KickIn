4808f82448a72a0f3211692e9613c3c3f57fc33c
junil1030 <dccrdseo@naver.com>
2025-12-16 10:14:29 +0900

    feat: Header에 필요한 apiKey와 ContentType 추가

--- KickIn/Core/Network/Helper/APIHeader.swift
+++ KickIn/Core/Network/Helper/APIHeader.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Alamofire
+
+enum APIHeader {
+    case apiKey
+    case applicationJSON
+}
+
+extension APIHeader {
+    var httpHeader: HTTPHeader {
+        switch self {
+        case .apiKey:
+            return HTTPHeader(name: "SeSACKey", value: APIConfig.apikey)
+            
+        case .applicationJSON:
+            return HTTPHeader(name: "Content-Type", value: "applicationJSON")
+        }
+    }
+}
