68d6b5392e8e72cab54fc03eecd1017b2bbbe166
junil1030 <dccrdseo@naver.com>
2025-12-16 12:59:24 +0900

    feat: APIHeader에 refreshToken, multipartFormData 케이스 추가

--- KickIn/Core/Network/Helper/APIHeader.swift
+++ KickIn/Core/Network/Helper/APIHeader.swift
@@ -11,6 +11,8 @@ import Alamofire
 enum APIHeader {
     case apiKey
     case applicationJSON
+    case refreshToken(String)
+    case multipartFormData
 }
 
 extension APIHeader {
@@ -18,9 +20,16 @@ extension APIHeader {
         switch self {
         case .apiKey:
             return HTTPHeader(name: "SeSACKey", value: APIConfig.apikey)
-            
+
         case .applicationJSON:
             return HTTPHeader(name: "Content-Type", value: "applicationJSON")
+
+        case .refreshToken(let token):
+            return HTTPHeader(name: "RefreshToken", value: token)
+
+        case .multipartFormData:
+            // Note: Actual boundary will be set by Alamofire's upload(multipartFormData:)
+            return HTTPHeader(name: "Content-Type", value: "multipart/form-data")
         }
     }
 }
