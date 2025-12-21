01942b716dd89ca01f29b495f5662ef873cec05b
junil1030 <dccrdseo@naver.com>
2025-12-21 22:52:15 +0900

    feat: HomeView에서 사용할 홈 상단 매물을 위한 UIModel 선언

--- KickIn/Features/Home/Models/TodayEstatesUIModel.swift
+++ KickIn/Features/Home/Models/TodayEstatesUIModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct TodayEstateUIModel {
+    let estateId: String?
+    let category: String?
+    let title: String?
+    let introduction: String?
+    let thumbnails: [String]?
+    let longitude: Double?
+    let latitude: Double?
+}
+
+extension TodayEstateItemDTO {
+    func toUIModel() -> TodayEstateUIModel {
+        return TodayEstateUIModel(
+            estateId: self.estateId,
+            category: self.category,
+            title: self.title,
+            introduction: self.introduction,
+            thumbnails: self.thumbnails,
+            longitude: self.geolocation?.longitude,
+            latitude: self.geolocation?.latitude
+        )
+    }
+}
