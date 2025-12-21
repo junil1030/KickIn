a444e43da3e6524581f1a813c327020ca666096c
junil1030 <dccrdseo@naver.com>
2025-12-21 22:48:42 +0900

    feat: hex 값을 사용하여 컬러 지정가능하도록 생성자 추가 및 기본 컬러 타입 프로퍼티 추가

--- KickIn/Shared/Utilities/Color+Extension.swift
+++ KickIn/Shared/Utilities/Color+Extension.swift
@@ -5,4 +5,50 @@
 //  Created by 서준일 on 12/19/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String, opacity: Double = 1.0) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let r, g, b: UInt64
+        switch hex.count {
+        case 6:
+            (r, g, b) = (
+                (int >> 16) & 0xFF,
+                (int >> 8) & 0xFF,
+                int & 0xFF
+            )
+        default:
+            (r, g, b) = (0, 0, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: opacity
+        )
+    }
+    
+    // MARK: - Brand Color
+    static let deepCream = Color(hex: "#DAC8B1")
+    static let brightCream = Color(hex: "#F1DCC1")
+    static let deepCoast = Color(hex: "#298E9C")
+    static let brightCoast = Color(hex: "#71C9D6")
+    static let deepWood = Color(hex: "#402A32")
+    static let brightWood = Color(hex: "#8C5543")
+    
+    // MARK: - Gray Scale Color
+    static let gray0 = Color(hex: "#FFFFFF")
+    static let gray15 = Color(hex: "#F9F9F9")
+    static let gray30 = Color(hex: "#EAEAEA")
+    static let gray45 = Color(hex: "#D8D6D7")
+    static let gray60 = Color(hex: "#ABABAE")
+    static let gray75 = Color(hex: "#6A6A6E")
+    static let gray90 = Color(hex: "#434347")
+    static let gray100 = Color(hex: "#0B0B0B")
+}
