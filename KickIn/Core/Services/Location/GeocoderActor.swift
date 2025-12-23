20ad8fef1616625cfe78725fa903a045baac66e6
junil1030 <dccrdseo@naver.com>
2025-12-23 14:24:19 +0900

    feat: CLGeocoder의 동시 호출 문제를 해결하기 위해 actor로 직렬화

--- KickIn/Core/Services/Location/GeocoderActor.swift
+++ KickIn/Core/Services/Location/GeocoderActor.swift
@@ -5,4 +5,16 @@
 //  Created by 서준일 on 12/23/25.
 //
 
-import Foundation
+import CoreLocation
+
+actor GeocoderActor {
+    
+    private let geocoder = CLGeocoder()
+    
+    func reverseGeocode(latitude: Double, longitude: Double) async throws -> CLPlacemark? {
+
+        let location = CLLocation(latitude: latitude, longitude: longitude)
+        let placemarks = try await geocoder.reverseGeocodeLocation(location)
+        return placemarks.first
+    }
+}
