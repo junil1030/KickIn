7b5c7a7212ae6f552dcb3395ecdcfbdf686e3abf
junil1030 <dccrdseo@naver.com>
2026-01-12 12:00:49 +0900

    feat: 네트워크 업로드 진행률 추적 기능 추가
    NetworkService에 uploadWithProgress 메서드 구현:
    - Alamofire uploadProgress를 통한 실시간 진행률 추적
    - MainActor에서 progressHandler 실행 보장
    - MockNetworkService에 테스트용 구현 추가

--- KickIn/Core/Network/Foundation/Service/NetworkServiceProtocol.swift
+++ KickIn/Core/Network/Foundation/Service/NetworkServiceProtocol.swift
@@ -11,4 +11,9 @@ protocol NetworkServiceProtocol {
     func request<T: Decodable>(_ router: any APIRouter) async throws -> T
     func request(_ router: any APIRouter) async throws
     func upload<T: Decodable>(_ router: any APIRouter, files: [(data: Data, name: String, fileName: String, mimeType: String)]) async throws -> T
+    func uploadWithProgress<T: Decodable>(
+        _ router: any APIRouter,
+        files: [(data: Data, name: String, fileName: String, mimeType: String)],
+        progressHandler: @escaping (Double) -> Void
+    ) async throws -> T
 }
