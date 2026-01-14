6bdb29c4d6f5f432ebcea2283f7f8f8eaebbc54f
junil1030 <dccrdseo@naver.com>
2026-01-14 19:37:22 +0900

    feat: Authenticator 추가

--- KickIn/Core/Network/Foundation/Authentication/KickInAuthenticationCredential.swift
+++ KickIn/Core/Network/Foundation/Authentication/KickInAuthenticationCredential.swift
@@ -1,5 +1,5 @@
 //
-//  KickInCredential.swift
+//  KickInAuthenticationCredential.swift
 //  KickIn
 //
 //  Created by 서준일 on 1/14/26.
@@ -8,7 +8,7 @@
 import Foundation
 import Alamofire
 
-struct KickInCredential: AuthenticationCredential {
+struct KickInAuthenticationCredential: AuthenticationCredential, Sendable {
     let accessToken: String
     let refreshToken: String
     let expiration: Date
--- KickIn/Core/Network/Foundation/Authentication/KickInAuthenticator.swift
+++ KickIn/Core/Network/Foundation/Authentication/KickInAuthenticator.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Alamofire
+
+final class KickInAuthenticator: Authenticator {
+    func refresh(_ credential: KickInAuthenticationCredential, for session: Alamofire.Session, completion: @escaping @Sendable (Result<KickInAuthenticationCredential, any Error>) -> Void) {
+        return
+    }
+    
+    func apply(_ credential: KickInAuthenticationCredential, to urlRequest: inout URLRequest) {
+        return
+    }
+    
+    func didRequest(_ urlRequest: URLRequest, with response: HTTPURLResponse, failDueToAuthenticationError error: any Error) -> Bool {
+        return true
+    }
+    
+    func isRequest(_ urlRequest: URLRequest, authenticatedWith credential: KickInAuthenticationCredential) -> Bool {
+        return true
+    }
+}
