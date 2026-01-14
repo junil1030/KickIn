c5d6904ed761ef8b2c163a097bd045eec1b3d557
junil1030 <dccrdseo@naver.com>
2026-01-14 21:59:34 +0900

    refactor: 기존 Interceptor 파일 정리
    - AuthenticationInterceptor.swift 삭제
    - TokenRefreshManager.swift 삭제
    - AuthHeaderProvider를 SeSACKey만 반환하도록 간소화
    - MockNetworkService 함수 추가

--- KickIn/Core/Network/Foundation/Interceptor/AuthHeaderProvider.swift
+++ KickIn/Core/Network/Foundation/Interceptor/AuthHeaderProvider.swift
@@ -8,31 +8,9 @@
 import Foundation
 import CachingKit
 
-/// Header provider that automatically injects authentication tokens and API keys
+/// Header provider that injects API key for CachingKit
 final class AuthHeaderProvider: HeaderProvider {
-    // MARK: - Properties
-
-    private let tokenStorage: any TokenStorageProtocol
-
-    // MARK: - Initialization
-
-    init(tokenStorage: any TokenStorageProtocol) {
-        self.tokenStorage = tokenStorage
-    }
-
-    // MARK: - HeaderProvider
-
     func headers() async -> [String: String] {
-        var headers: [String: String] = [:]
-
-        // Add API key
-        headers["SeSACKey"] = await APIConfig.apikey
-
-        // Add access token if available
-        if let accessToken = await tokenStorage.getAccessToken() {
-            headers["Authorization"] = accessToken
-        }
-
-        return headers
+        ["SeSACKey": APIConfig.apikey]
     }
 }
